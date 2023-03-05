5ace254334ea4783a6fff83112cc4ee58796f8db
Rifazul <rifazul50@gmail.com>
2023-03-05 08:26:39 +0600

    add admin system

--- index.js
+++ index.js
@@ -56,6 +56,44 @@ async function run(){
 
 
 
+    //  get check seller role Api
+
+    app.get('/users/admin/:email',async(req,res)=>{
+
+        const email = req.params.email;
+        const query = {email}
+        const user = await usersCollection.findOne(query)
+
+        res.send({ isAdmin: user?.role === 'admin'})
+  })
+
+
+
+
+
+   app.get('/users/buyer/:email',async(req,res)=>{
+
+     const email = req.params.email;
+     const query = {email}
+     const user = await usersCollection.findOne(query)
+
+     res.send({ isBuyer: user?.role === 'buyer'})
+})
+
+
+
+    //  get check seller role Api
+
+    app.get('/users/seller/:email',async(req,res)=>{
+
+        const email = req.params.email;
+         const query = {email}
+         const user = await usersCollection.findOne(query)
+
+         res.send({ isSeller: user?.role === 'seller'})
+   })
+
+
 //   post sellerProducts methoad 
     
 app.post('/sellerProducts',async(req,res)=>{
